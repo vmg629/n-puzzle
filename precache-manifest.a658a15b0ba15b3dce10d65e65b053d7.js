845a2944ffe23f83646852a5347a3bf6118b56c5
vmg629 <vmg629@gmail.com>
2020-09-21 21:01:01 +0200

    Updates

---
+++ precache-manifest.a658a15b0ba15b3dce10d65e65b053d7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a7ded9390cf41c993fb4afceec02403c",
+    "url": "/n-puzzle/index.html"
+  },
+  {
+    "revision": "a3ee4622e56265265fdd",
+    "url": "/n-puzzle/static/css/main.833a8fd5.chunk.css"
+  },
+  {
+    "revision": "bb1b9ae61ab65cdc5a90",
+    "url": "/n-puzzle/static/js/2.510a0c45.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/n-puzzle/static/js/2.510a0c45.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a3ee4622e56265265fdd",
+    "url": "/n-puzzle/static/js/main.33f6c8f9.chunk.js"
+  },
+  {
+    "revision": "3de6f3e695c03e83895e",
+    "url": "/n-puzzle/static/js/runtime-main.d83f3910.js"
+  }
+]);
